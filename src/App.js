d8eea392e3ee1064f4ba88a00aa7f691c7fa1ac7
lohith <lohith@gmail.com>
2024-06-18 19:43:22 +0530

    gimme more

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './navbar';
 function App() {
   return (
     <div className="App">
-        
+         <Navbar />
     </div>
   );
 }
---
+++ src/navbar.js
@@ -0,0 +1,18 @@
+import logo from './images/cost.png';
+import './navbar.css';
+const Navbar = () => {
+    return ( 
+       <nav className="navbar">
+        <div className="logoclass"><img src={logo} alt="logo" className="logo" /> <p>expense tracker</p></div>
+        <ul className="listitems">
+            <li><a href="">Home</a></li>
+            <li><a href="">Expense List</a></li>
+            <li><a href="">Add Expense</a></li>
+            <li><a href="">Contact Us</a></li>
+            <li><a href="">more info</a></li>
+        </ul>
+       </nav>
+     );
+}
+ 
+export default Navbar;
