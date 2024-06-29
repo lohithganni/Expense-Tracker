456292fd1305a121d996374dbe9781f49fd7a41e
lohithganni <116790357+lohithganni@users.noreply.github.com>
2024-06-29 23:29:02 +0530

    Update home.js

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,11 +1,11 @@
 import Navbar from "../components/navbar";
 import TodayExpenses from "../components/todayExpense";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Add from "../components/add";
 
 const Home = () => {
     const MAIN_OBJECT_KEY = "mainObject";
-    const [mainObject, setMainObject] = useState(() => {
+    const [mainObject] = useState(() => {
         const storedMainObject = localStorage.getItem(MAIN_OBJECT_KEY);
         if (storedMainObject) {
           try {
